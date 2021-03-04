5af5969a90c72db8eb1c4be8efee5298d3793d39
Oliver Leitner <never@neverslair-blog.net>
2021-03-04 21:04:57 +0100

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+// using Microsoft.AspNetCore.TestHost;
+// using Microsoft.AspNetCore;
+// using System.Threading.Tasks;
+// using System.Net.Http;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.Hosting;
+// using DotNetCoreSampleApi;
+
+namespace DotNetCoreSampleApiTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
